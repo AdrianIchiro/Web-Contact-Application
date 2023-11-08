0c30f27be928fbbc1b0b3762beb27a48bb8d0c08
AdrianIchiro <adrianfahren10@gmail.com>
2023-11-08 17:12:29 +0700

    Express Layout

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express')
-
+const expressLayout = require('express-layouts')
 
 const app = express()
 const port = 3000
 
+app.use(expressLayout)
+app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
     res.send('Hello World')
